f8d8e97ca4817e8fbd5188063cc670bcdf3dcd1f
Evgeny Pavlusenko <evgenyp2004@gmail.com>
2022-11-06 21:43:00 +0500

    Добавлена аутентификация с помощью гугла. Для справки: Файлы LauncherActivity.kt (Его трогайте с моего ведома) и activity_launcher.xml относятся к экрану логина, где находится кнопка войти. В файлах MainActivity.kt и activity_main.xml есть инфа о юзере и кнопка выйти. Но вообще это может быть одним из главных экранов. Короче, разберетесь.

--- app/src/main/java/com/android/studentdiary/MainActivity.kt
+++ app/src/main/java/com/android/studentdiary/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.android.studentdiary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        val email = intent.getStringExtra("email")
+        val displayname = intent.getStringExtra("name")
+
+        findViewById<TextView>(R.id.textView).text = email + "\n" + displayname
+
+        findViewById<Button>(R.id.bSignOut).setOnClickListener{
+            auth.signOut()
+            startActivity(Intent(this, LauncherActivity::class.java))
+        }
+
+
+
+
+
+        /*val db = Firebase.firestore
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+        db.collection("users").add(user)*/
+            /*.addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }*/
     }
 }
+
